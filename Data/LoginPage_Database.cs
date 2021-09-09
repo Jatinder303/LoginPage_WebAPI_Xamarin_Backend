b16779ffa4e3fa10c448e3d621588a16e6a9d062
Jatinder Singh <jatinder303@gmail.com>
2021-09-10 11:15:04 +1200

    Add project files.

---
+++ Data/LoginPage_Database.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LoginPage_WebAPI_Xamarin_Backend.Models;
+
+namespace LoginPage_WebAPI_Xamarin_Backend.Data
+{
+    public class LoginPage_Database : DbContext
+    {
+        public LoginPage_Database (DbContextOptions<LoginPage_Database> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LoginPage_WebAPI_Xamarin_Backend.Models.Login> Login { get; set; }
+    }
+}
---
+++ Migrations/20210909005400_Login.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LoginPage_WebAPI_Xamarin_Backend.Migrations
+{
+    public partial class Login : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Login",
+                columns: table => new
+                {
+                    Username = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Login", x => x.Username);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Login");
+        }
+    }
+}
